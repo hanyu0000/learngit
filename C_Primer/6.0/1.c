6c77e6e7c215e3bdd5a91223e9fa33e750ccf338
hanyu0000 <3634802280@qq.com>
2023-11-19 19:15:26 +0800

    6.0

---
+++ C_Primer/6.0/1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/*int main(){
+    for(int i = 1;i<=5;i++){
+        int j = 1;
+        while(j <=i){
+            printf("#");
+            j++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main(){
+    int a = 70;
+    for(int i = 0;i<6;i++){
+        for(int j = a;j>=70-i;j--)
+            printf("%c",j);
+        printf("\n");
+    }
+    return 0;
+}*/
+int main(){
+    int c = 'A';
+    for(int i = 1;i<=6;i++){
+        for(int j = 0;j<i;j++,c++)
+            printf("%c",c);
+        printf("\n");
+    }
+    return 0;
+}
